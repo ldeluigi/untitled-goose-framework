9f71e8385f78ded5c81785d77387717c16030f26
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-07-20 10:11:49 +0200

    add dialog for final move

--- src/main/scala/model/actions/StepForwardAction.scala
+++ src/main/scala/model/actions/StepForwardAction.scala
@@ -2,6 +2,8 @@ package model.actions
 
 import model.entities.board.Position
 import model.{MatchState, Tile}
+import scalafx.scene.control.Alert
+import scalafx.scene.control.Alert.AlertType
 
 class StepForwardAction() extends Action {
 
@@ -12,6 +14,9 @@ class StepForwardAction() extends Action {
     val nextTile = gameState.matchBoard.next(piece.position.tile)
     if (nextTile.isDefined) {
       piece.setPosition(Position(nextTile.get))
+    } else {    //TODO move to view
+      new Alert(AlertType.Information, "You won! Game over.").showAndWait()
+
     }
     gameState
   }
