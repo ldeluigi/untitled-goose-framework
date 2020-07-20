a115930abfca274bed20f934f13a2cf6c491f44f
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-07-20 21:37:33 +0200

    Add ratio

--- src/main/scala/model/entities/board/Disposition.scala
+++ src/main/scala/model/entities/board/Disposition.scala
@@ -13,13 +13,15 @@ trait Disposition {
 
 object Disposition {
 
-  private class SnakeDisposition(val totalTiles: Int, val ratio : Int = 1) extends Disposition {
-    val side: Int = Math.sqrt(totalTiles).ceil.toInt
+  private abstract class BaseDisposition(val totalTiles: Int, val ratio: Int) extends Disposition {
+    val height: Double = Math.sqrt(totalTiles / ratio)
 
-    override def rows: Int = side
+    override def rows: Int = height.ceil.toInt
 
-    override def columns: Int = side
+    override def columns: Int = (totalTiles / height).ceil.toInt
+  }
 
+  private class SnakeDisposition(totalTiles: Int, ratio: Int) extends BaseDisposition(totalTiles, ratio) {
     override def tilePlacement(tileIndex: Int): (Int, Int) = {
       val rowIndex = tileIndex / columns
       val columnIndex = tileIndex % columns
@@ -28,22 +30,20 @@ object Disposition {
     }
   }
 
-  private class SpiralDisposition(val totalTiles: Int, val ratio: Int = 1) extends Disposition {
-    val side: Int = Math.sqrt(totalTiles).ceil.toInt
-
-    override def rows: Int = side
-
-    override def columns: Int = side
+  private class SpiralDisposition(totalTiles: Int, ratio: Int) extends BaseDisposition(totalTiles, ratio) {
 
     private val borderLength = Math.max((columns + rows) * 2 - 4, 1)
 
     override def tilePlacement(tileIndex: Int): (Int, Int) = {
       def _tilePlacement(tileIndex: Int, rows: Int, columns: Int): (Int, Int) = {
         if (tileIndex < borderLength)
-          if (tileIndex < columns) (tileIndex, rows - 1) else
-            if (tileIndex < columns + rows - 1) (columns - 1, rows + columns - tileIndex - 2) else
-              if (tileIndex < columns * 2 + rows - 2) (rows + columns * 2 - tileIndex - 3, 0) else
-            (0, tileIndex - rows - columns * 2 + 3)
+          if (tileIndex < columns)
+            (tileIndex, rows - 1)
+          else if (tileIndex < columns + rows - 1)
+            (columns - 1, rows + columns - tileIndex - 2)
+          else if (tileIndex < columns * 2 + rows - 2)
+            (rows + columns * 2 - tileIndex - 3, 0)
+          else (0, tileIndex - rows - columns * 2 + 3)
         else
           _tilePlacement(tileIndex - borderLength, rows - 2, columns - 2) match {
             case (x, y) => (x + 1, y + 1)
@@ -90,9 +90,9 @@ object Disposition {
     }
   }
 
-  def snake(total: Int): Disposition = new SnakeDisposition(total)
+  def snake(total: Int, ratio: Int = 1): Disposition = new SnakeDisposition(total, ratio)
 
-  def spiral(total: Int): Disposition = new SpiralDisposition(total)
+  def spiral(total: Int, ratio: Int = 1): Disposition = new SpiralDisposition(total, ratio)
 
   def loop(total: Int): Disposition = new LoopDisposition(total)
 
--- src/test/scala/model/entities/board/DispositionTest.scala
+++ src/test/scala/model/entities/board/DispositionTest.scala
@@ -17,10 +17,22 @@ class DispositionTest extends AnyFlatSpec with Matchers {
     sd.columns should be(10)
   }
 
-  "Snake disposition" should "place correctly" in {
+  "Snake disposition" should "place correctly a square" in {
     val sd = Disposition.snake(16)
     val list: List[(Int, Int)] = Range(0, 16).map(sd.tilePlacement).toList
     list should be(List((0, 3), (1, 3), (2, 3), (3, 3), (3, 2), (2, 2), (1, 2), (0, 2), (0, 1), (1, 1), (2, 1), (3, 1), (3, 0), (2, 0), (1, 0), (0, 0)))
+  }
+
+  "Snake disposition" should "place correctly an incomplete square" in {
+    val sd = Disposition.snake(15)
+    val list: List[(Int, Int)] = Range(0, 15).map(sd.tilePlacement).toList
+    list should be(List((0, 3), (1, 3), (2, 3), (3, 3), (3, 2), (2, 2), (1, 2), (0, 2), (0, 1), (1, 1), (2, 1), (3, 1), (3, 0), (2, 0), (1, 0)))
+  }
+
+  "Snake disposition" should "place correctly an incomplete rectangle" in {
+    val sd = Disposition.snake(15, 2)
+    val list: List[(Int, Int)] = Range(0, 15).map(sd.tilePlacement).toList
+    list should be(List((0,2), (1,2), (2,2), (3,2), (4,2), (5,2), (5,1), (4,1), (3,1), (2,1), (1,1), (0,1), (0,0), (1,0), (2,0)))
   }
 
   "Spiral disposition" should "have squared rows" in {
@@ -33,9 +45,21 @@ class DispositionTest extends AnyFlatSpec with Matchers {
     sd.columns should be(10)
   }
 
-  "Spiral disposition" should "place correctly" in {
+  "Spiral disposition" should "place correctly a square" in {
     val sd = Disposition.spiral(16)
     val list: List[(Int, Int)] = Range(0, 16).map(sd.tilePlacement).toList
     list should be(List[(Int, Int)]((0, 3), (1, 3), (2, 3), (3, 3), (3, 2), (3, 1), (3, 0), (2, 0), (1, 0), (0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (2, 1), (1, 1)))
+  }
+
+  "Spiral disposition" should "place correctly an incomplete square" in {
+    val sd = Disposition.spiral(15)
+    val list: List[(Int, Int)] = Range(0, 15).map(sd.tilePlacement).toList
+    list should be(List[(Int, Int)]((0, 3), (1, 3), (2, 3), (3, 3), (3, 2), (3, 1), (3, 0), (2, 0), (1, 0), (0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (2, 1)))
+  }
+
+  "Spiral disposition" should "place correctly an incomplete rectangle" in {
+    val sd = Disposition.spiral(15, 2)
+    val list: List[(Int, Int)] = Range(0, 15).map(sd.tilePlacement).toList
+    list should be(List((0,2), (1,2), (2,2), (3,2), (4,2), (5,2), (5,1), (5,0), (4,0), (3,0), (2,0), (1,0), (0,0), (0,1), (1,1)))
   }
 }
