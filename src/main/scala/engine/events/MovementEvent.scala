ba05723680e857cad0ed9caf81bddc1087c340b4
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2020-07-22 12:08:27 +0200

    Add changes to MovementEvent

--- src/main/scala/engine/events/MovementEvent.scala
+++ src/main/scala/engine/events/MovementEvent.scala
@@ -2,12 +2,12 @@ package engine.events
 
 import com.sun.media.jfxmedia.events.PlayerEvent
 
-class MovementEvent() extends PlayerEvent {
+abstract class MovementEvent() extends PlayerEvent {
   def movement: Int //TO DO
 }
 
 object MovementEvent{
-  def apply(): MovementEvent = new MovementEvent()
+  //To Do Apply
 }
 
 
