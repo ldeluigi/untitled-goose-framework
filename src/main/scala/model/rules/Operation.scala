5afe8280d24083ed6f3ea2e5fccb7748f892ba07
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-07-22 09:46:58 +0200

    update Operation with execute

--- src/main/scala/model/rules/Operation.scala
+++ src/main/scala/model/rules/Operation.scala
@@ -1,13 +1,34 @@
 package model.rules
 
+import model.{MatchState}
+
 trait Operation {
+
   def possibleOperations: Set[String]
+
+  def checkOperation(operation: String): Boolean
+
+  def solveOperation: MatchState
+
+  def execute(state: MatchState)
+
 }
 
 object Operation {
 
+  // TODO import operations stack from engine and update constructor accordingly
+
   private class OperationImpl() extends Operation {
-    def possibleOperations: Set[String] = Set()
+
+    override def possibleOperations: Set[String] = Set()
+
+    override def checkOperation(operation: String): Boolean = {
+      possibleOperations.contains(operation)
+    }
+
+    override def solveOperation: MatchState = ???
+
+    override def execute(state: MatchState): Unit = ???
   }
 
   def apply(): Operation = new OperationImpl()
