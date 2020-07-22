87f60c1da29562f7f9e398cc0627977ed8216a00
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2020-07-22 11:23:53 +0200

    Create engine class extends GameEvent

--- src/main/scala/model/GameEvent.scala
+++ src/main/scala/model/GameEvent.scala
@@ -2,10 +2,19 @@ package model
 
 trait GameEvent {
 
-  def persistent: Boolean
-
   def name: String
 
   def group: List[String]
 
 }
+
+object GameEvent{
+  private class GameEventImpl(nameEvent: String) extends GameEvent{
+    override def name: String = nameEvent
+
+    override def group: List[String] = ???
+  }
+
+  def apply(nameEvent: String): GameEvent = new GameEventImpl(nameEvent)
+}
+
