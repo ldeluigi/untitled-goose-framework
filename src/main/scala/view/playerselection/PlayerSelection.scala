5f87e69432540438218cd6e0b5fea6b6171142cb
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-08-11 17:37:33 +0200

    add upper pane to contain game's name

--- src/main/scala/view/playerselection/PlayerSelection.scala
+++ src/main/scala/view/playerselection/PlayerSelection.scala
@@ -7,12 +7,14 @@ import model.{Color, Player}
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ColorPicker, Label, TextField}
-import scalafx.scene.layout.{AnchorPane, BorderPane, HBox}
+import scalafx.scene.layout.{AnchorPane, Background, BackgroundFill, BorderPane, HBox}
+import scalafx.scene.paint.Color.{ALICEBLUE, BLUE, Blue, LightGreen, color}
 import scalafx.stage.Stage
 import view.ApplicationController
 
 class PlayerSelection(stage: Stage, board: Board, ruleSet: RuleSet, widthSize: Int, heightSize: Int) extends Scene {
 
+  val players: Map[Player, Piece] = Map(Player("P1") -> Piece(Color.Red), Player("P2") -> Piece(Color.Blue))
   val borderPane = new BorderPane()
   val anchorPane = new AnchorPane()
 
@@ -22,12 +24,20 @@ class PlayerSelection(stage: Stage, board: Board, ruleSet: RuleSet, widthSize: I
   val playerNameFromInput = new TextField()
   val colorsChoice = new ColorPicker()
   val addPlayer = new Button("Add player")
-  val start = new Button("Start game!")
+  val startGame = new Button("Start game!")
   val cancel = new Button("Cancel")
+
+  val upperGameNameHeader: HBox = new HBox() {
+    alignment = Pos.Center
+    padding = Insets(30)
+    fill = Blue
+    children = new Label("Game Name") // TODO get the actual name of the game
+  }
 
   val centerPlayerConsole: HBox = new HBox() {
     alignment = Pos.Center
-    spacing = 10
+    spacing = 30
+    padding = Insets(30)
     children = List(playerName, playerNameFromInput, colorsChoice, addPlayer)
   }
 
@@ -35,21 +45,19 @@ class PlayerSelection(stage: Stage, board: Board, ruleSet: RuleSet, widthSize: I
     alignment = Pos.BottomRight
     spacing = 30
     padding = Insets(30)
-    children = List(start, cancel)
+    children = List(startGame, cancel)
   }
 
-  borderPane.top = new Label("Game name")
+  borderPane.top = upperGameNameHeader
   borderPane.center = centerPlayerConsole
   borderPane.bottom = bottomGameControls
-
-  val players: Map[Player, Piece] = Map(Player("P1") -> Piece(Color.Red), Player("P2") -> Piece(Color.Blue))
 
   addPlayer.onAction = _ => {
     playerNameFromInput.clear()
     // add entry to players map
   }
 
-  start.onAction = _ => {
+  startGame.onAction = _ => {
     val currentMatch: Match = Match(board, players, ruleSet)
     val appView: ApplicationController = ApplicationController(stage, widthSize, heightSize, currentMatch)
   }
