aa2d669219c38d07b2c686d3f99d899b4b88ebc5
Samuele Burattini <samuele.burattini@studio.unibo.it>
2020-08-11 12:43:08 +0200

    Refactor of groups

--- src/main/scala/engine/events/root/NoOpEvent.scala
+++ src/main/scala/engine/events/root/NoOpEvent.scala
@@ -3,11 +3,11 @@ package engine.events.root
 case object NoOpEvent extends GameEvent {
   override def name: String = ""
 
-  override def group: List[String] = List()
-
   override def turn: Int = -1
 
   override def isConsumed: Boolean = false
 
   override def consume(): Unit = {}
+
+  override def groups: Set[String] = Set()
 }
--- src/main/scala/model/entities/board/TileDefinition.scala
+++ src/main/scala/model/entities/board/TileDefinition.scala
@@ -1,11 +1,11 @@
 package model.entities.board
 
-trait TileDefinition {
+import model.Groupable
+
+trait TileDefinition extends Groupable {
   def number: Option[Int]
 
   def name: Option[String]
-
-  def tileType: Option[List[String]]
 }
 
 
@@ -19,23 +19,21 @@ object TileDefinition {
       case (Some(xNum), Some(yNum)) => xNum compare yNum
     }
 
-  private class TileDefinitionImpl(val number: Option[Int], val name: Option[String]) extends TileDefinition {
-
-    override def tileType: Option[List[String]] = None
+  private class TileDefinitionImpl(val number: Option[Int], val name: Option[String], val groups: Set[String]) extends TileDefinition {
 
     override def equals(obj: Any): Boolean = {
       obj match {
         case t: TileDefinition =>
           (number.isDefined && t.number.isDefined && number.get == t.number.get) ||
-          (name.isDefined && t.name.isDefined && name.get == t.name.get)
+            (name.isDefined && t.name.isDefined && name.get == t.name.get)
         case _ => false
       }
     }
   }
 
-  def apply(number: Int): TileDefinition = new TileDefinitionImpl(Some(number), None)
+  def apply(number: Int, groups: Set[String] = Set()): TileDefinition = new TileDefinitionImpl(Some(number), None, groups)
 
-  def apply(name: String): TileDefinition = new TileDefinitionImpl(None, Some(name))
+  def apply(name: String, groups: Set[String] = Set()): TileDefinition = new TileDefinitionImpl(None, Some(name), groups)
 
-  def apply(number: Int, name: String): TileDefinition = new TileDefinitionImpl(Some(number), Some(name))
+  def apply(number: Int, name: String, groups: Set[String] = Set()): TileDefinition = new TileDefinitionImpl(Some(number), Some(name), groups)
 }
