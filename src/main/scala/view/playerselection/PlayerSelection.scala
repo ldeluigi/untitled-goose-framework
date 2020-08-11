c526c0ca8a7451f288d29658e1426d26bb1f8761
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-08-11 16:26:09 +0200

    finish intro GUI disposition, to be refined

--- src/main/scala/view/playerselection/PlayerSelection.scala
+++ src/main/scala/view/playerselection/PlayerSelection.scala
@@ -4,42 +4,58 @@ import engine.`match`.Match
 import model.entities.board.{Board, Piece}
 import model.rules.ruleset.RuleSet
 import model.{Color, Player}
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.control.{Button, ColorPicker, ComboBox, Label, TextField}
-import scalafx.scene.layout.{BorderPane, HBox}
+import scalafx.scene.control.{Button, ColorPicker, Label, TextField}
+import scalafx.scene.layout.{AnchorPane, BorderPane, HBox}
 import scalafx.stage.Stage
 import view.ApplicationController
 
 class PlayerSelection(stage: Stage, board: Board, ruleSet: RuleSet, widthSize: Int, heightSize: Int) extends Scene {
 
   val borderPane = new BorderPane()
+  val anchorPane = new AnchorPane()
+
   root = borderPane
 
   val playerName = new Label("Insert player name: ")
   val playerNameFromInput = new TextField()
   val colorsChoice = new ColorPicker()
-  val addPlayer = new Button("Add player to the game")
-  val startGame = new Button("Start game!")
+  val addPlayer = new Button("Add player")
+  val start = new Button("Start game!")
+  val cancel = new Button("Cancel")
 
-  val innerSelection = new HBox(playerName, playerNameFromInput, colorsChoice, addPlayer)
-  innerSelection.spacing = 5
+  val centerPlayerConsole = new HBox() {
+    alignment = Pos.Center
+    spacing = 10
+    children = List(playerName, playerNameFromInput, colorsChoice, addPlayer)
+  }
+
+  val bottomGameControls = new HBox() {
+    alignment = Pos.BottomRight
+    spacing = 30
+    padding = Insets(30)
+    children = List(start, cancel)
+  }
 
   borderPane.top = new Label ("Game name")
-  borderPane.center = innerSelection
-  borderPane.bottom = startGame
+  borderPane.center = centerPlayerConsole
+  borderPane.bottom = bottomGameControls
 
-
-
-  //Generates an entry of this map whenever pressing addPlayer Button
   val players: Map[Player, Piece] = Map(Player("P1") -> Piece(Color.Red), Player("P2") -> Piece(Color.Blue))
 
   addPlayer.onAction = (_) => {
-
+    playerNameFromInput.clear()
+    // add entry to players map
   }
 
-  startGame.onAction = (_) => {
+  start.onAction = (_) => {
     val currentMatch: Match = Match(board, players, ruleSet)
     val appView: ApplicationController = ApplicationController(widthSize, heightSize, currentMatch)
+  }
+
+  cancel.onAction = (_) => {
+    playerNameFromInput.clear()
   }
 
 }
