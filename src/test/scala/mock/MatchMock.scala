e089e4330e65d920cd7f380a93b897eedf4c2492
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-08-12 00:00:24 +0200

    Add MatchMock

---
+++ src/test/scala/mock/MatchMock.scala
@@ -0,0 +1,14 @@
+package mock
+
+import engine.`match`.Match
+import model.{Color, Player}
+import model.entities.board.{Board, Disposition, Piece}
+import model.rules.ruleset.{PlayerOrdering, PriorityRuleSet}
+
+object MatchMock {
+  val board: Board = Board(10, Disposition.snake(10))
+  val p1: Player = Player("P1")
+  val p2: Player = Player("P2")
+  val players: Map[Player, Piece] = Map(p1 -> Piece(Color.Red), p2 -> Piece(Color.Blue))
+  val default: Match = Match(board, players, PriorityRuleSet(playerOrdering = PlayerOrdering.givenOrder(Seq(p1, p2))))
+}
