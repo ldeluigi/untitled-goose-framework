51fe49ee4840c5e6fee76dae313b6d23c5959651
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-08-11 20:35:42 +0200

    Remove pointless abstract keyword

--- src/main/scala/engine/events/root/ConsumableGameEvent.scala
+++ src/main/scala/engine/events/root/ConsumableGameEvent.scala
@@ -1,6 +1,6 @@
 package engine.events.root
 
-abstract class ConsumableGameEvent(currentTurn: Int) extends PersistentGameEvent(currentTurn) {
+class ConsumableGameEvent(currentTurn: Int) extends PersistentGameEvent(currentTurn) {
   private[this] var consumed: Boolean = false
 
   override def isConsumed: Boolean = consumed
