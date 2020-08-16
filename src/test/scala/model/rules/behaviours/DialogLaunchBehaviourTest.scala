13acb59670d2be3c0b2e71d07f21e76db88ed2f3
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-08-16 13:13:51 +0200

    Add DialogLaunchBehaviourTest

---
+++ src/test/scala/model/rules/behaviours/DialogLaunchBehaviourTest.scala
@@ -0,0 +1,25 @@
+package model.rules.behaviours
+
+import engine.events.DialogLaunchEvent
+import mock.MatchMock
+import model.entities.DialogContent
+import model.game.Game
+import model.rules.operations.SpecialOperation.DialogOperation
+import model.rules.ruleset.PriorityRuleSet
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.Seq
+
+class DialogLaunchBehaviourTest extends AnyFlatSpec with Matchers {
+
+  behavior of "DialogLaunchBehaviourTest"
+
+  it should "consume and convert Dialog Events into operations" in {
+    val m: Game = MatchMock.default
+    val e = DialogLaunchEvent(m.currentState.currentTurn, _ => DialogContent("", ""))
+    m.submitEvent(e)
+    DialogLaunchBehaviour().applyRule(m.currentState) should equal (Seq(DialogOperation(e.createDialog)))
+  }
+
+}
