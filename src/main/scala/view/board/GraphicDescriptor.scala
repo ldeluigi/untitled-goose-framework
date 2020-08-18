1937508e6bf5dbeee1abbf6fa7c145952ed38961
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-08-18 16:24:31 +0200

    Fix GraphicDescriptor - wrong type of map

--- src/main/scala/view/board/GraphicDescriptor.scala
+++ src/main/scala/view/board/GraphicDescriptor.scala
@@ -1,7 +1,7 @@
 package view.board
 
 import model.TileIdentifier
-import scalafx.scene.shape.Shape
+import scalafx.scene.paint.Color
 
 //conversione impliccita e mappa nella view con graphic descriptor dentro cui metto la conversione implicita che usa direttamente sclafx
 //capire come fare le conversioni implicite da num a graphic.. da nome a graphic..
@@ -17,27 +17,25 @@ import scalafx.scene.shape.Shape
 //poi guardare nella view come gestire queste cose, leggendo la mappa e convertirle in aspetti grafici con le priorità seguenti -> nome > gruppo > num
 
 trait GraphicDescriptor {
-  //c'è una mappa da tileidentifier a graphicdescriptor ->
+
+  // c'è una mappa da tileidentifier a graphicdescriptor ->
   // implicit conversion che dice che una tupla numero group qualcosa diventa graphicdescriptor
   // creo tante conversioni implicite da tupla sopra a sotto...
 
-  implicit def map(tileIdentifier: TileIdentifier): GraphicDescriptor
+  var graphicMap: Map[TileIdentifier, GraphicDescriptor] = Map()
 
-  def color: Shape
+  val color: Color
 
 }
 
-object GraphicDescriptor{
+object GraphicDescriptor {
 
   //creare la classe gruppo
 
-  private class GraphicDescriptorImpl extends GraphicDescriptor{
+  private class GraphicDescriptorImpl extends GraphicDescriptor {
 
-    override implicit def map(tileIdentifier: TileIdentifier): GraphicDescriptor = {
-      ???
+    override val color: Color = ???
 
-    }
-
-    override def color: Shape = ???
   }
+
 }
