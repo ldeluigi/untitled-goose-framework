5185627381fd0f7c9bd0163fdfc1d13f5dd29f15
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-08-28 17:58:57 +0200

    Behaviour test setup

--- src/test/scala/model/rules/behaviours/DialogLaunchBehaviourTest.scala
+++ src/test/scala/model/rules/behaviours/DialogLaunchBehaviourTest.scala
@@ -1,5 +1,11 @@
 package model.rules.behaviours
 
+import engine.events.consumable.DialogLaunchEvent
+import mock.MatchMock
+import model.entities.DialogContent
+import model.game.GameStateExtensions.MutableStateExtensions
+import model.game.{Game, MutableGameState}
+import model.rules.operations.Operation
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -7,12 +13,20 @@ class DialogLaunchBehaviourTest extends AnyFlatSpec with Matchers {
 
   behavior of "DialogLaunchBehaviourTest"
 
-  /*it should "consume and convert Dialog Events into operations" in {
-    val m: Game = MatchMock.default
-    val e = DialogLaunchEvent(m.currentState.currentTurn, _ => DialogContent("", ""))
-    m.submitEvent(e)
-    DialogLaunchBehaviour().applyRule(m.currentState) should equal(Seq(DialogOperation(e.createDialog)))
-    DialogLaunchBehaviour().applyRule(m.currentState) should have size 0
-  }*/
+  it should "consume and convert Dialog Events into operations" in {
+    val game: Game = MatchMock.default
+    val event = DialogLaunchEvent(game.currentState.currentTurn, game.currentState.currentCycle, DialogContent("title", "content"))
+    val state: MutableGameState = game.currentState
+
+    state.submitEvent(event)
+
+    val operationSequence: Seq[Operation] = DialogLaunchBehaviour().applyRule(state)
+    operationSequence.foreach(_.execute(state))
+
+    state.consumableBuffer should contain theSameElementsAs operationSequence
+
+    state.consumableBuffer should have size 1
+
+  }
 
 }
--- src/test/scala/model/rules/behaviours/MultipleStepBehaviourTest.scala
+++ src/test/scala/model/rules/behaviours/MultipleStepBehaviourTest.scala
@@ -12,29 +12,19 @@ class MultipleStepBehaviourTest extends AnyFlatSpec with Matchers {
 
   behavior of "MultipleStepBehaviourTest"
 
-  it should "applyRule" in {
+  it should "apply rule and check if the game state has been altered correctly" in {
     val game: Game = MatchMock.default
     val event = StepMovementEvent(1, game.currentState.currentPlayer, game.currentState.currentTurn, game.currentState.currentCycle)
     val state: MutableGameState = game.currentState
 
     state.submitEvent(event)
 
-    val operationSequence: Seq[Operation] = MultipleStepBehaviour().applyRule(state) // punto 3
-    operationSequence.foreach(_.execute(state)) //punto 4 eseguo tutte le op
-    // guardo risultato di StepOperation, poi come queste cose alterano lo stato e verificare che ci siano
-    // in mock i giocatori sono in posizione nulla, quindi non lanciano l'evento
-    // controllare che il player si trovi nella tile di prima + 1
-    // evento entrato sulla tile, evento fermato sulla tile
-    // più condizioni in uno stesso test
+    val operationSequence: Seq[Operation] = MultipleStepBehaviour().applyRule(state)
+    operationSequence.foreach(_.execute(state))
 
-    state.consumableBuffer.contains()
+    state.consumableBuffer should contain theSameElementsAs operationSequence
 
-
-    // verificare modifica corretta di game state
-
-    //MultipleStepBehaviour().applyRule(m.currentState) should have size 0
-
+    // TODO check if player has moved by 1 tile and that it contains TileEnteredEvent and StopOnTileEvent
 
   }
-
 }
