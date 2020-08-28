9d9b0076dc4dabd748989f2e45559fa1c5b89fd5
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2020-08-28 11:17:59 +0200

    Add MutableGameStateTest.

--- src/test/scala/model/game/MutableGameStateTest.scala
+++ src/test/scala/model/game/MutableGameStateTest.scala
@@ -1,29 +1,40 @@
 package model.game
 
+import mock.MatchMock
+import model.entities.board.Piece
+import model.{Color, Player}
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class MutableGameStateTest extends AnyFlatSpec {
+class MutableGameStateTest extends AnyFlatSpec with Matchers {
+
+  var gameMatch: Game = MatchMock.default
+  val gameMutableState: MutableGameState = gameMatch.currentState
+
+  val p1: Player = Player("P1")
+  val piece: Piece = Piece(Color.Blue)
 
   behavior of "MutableGameStateTest"
 
   it should "currentTurn_$eq" in {
-    pending
+    gameMutableState.currentTurn should be(0)
   }
 
   it should "gameHistory_$eq" in {
-    pending
+    gameMutableState.gameHistory.size should be(0)
   }
 
   it should "currentPlayer_$eq" in {
-    pending
+    gameMutableState.currentPlayer should equal(p1)
   }
 
   it should "updatePlayerPiece" in {
-    pending
+    gameMutableState.updatePlayerPiece(p1, _ => piece)
+    gameMutableState.playerPieces(p1).color should equal(piece.color)
   }
 
   it should "consumableEvents_$eq" in {
-    pending
+    gameMutableState.consumableBuffer.size should be(0)
   }
 
 }
