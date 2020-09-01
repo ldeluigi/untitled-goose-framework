96a18ce440b9a4677234ada6247b76ca9c2578f0
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-09-01 19:08:13 +0200

    Add SkipTurnBehaviourTest - to complete, list is coming back empty

--- src/test/scala/model/rules/behaviours/SkipTurnBehaviourTest.scala
+++ src/test/scala/model/rules/behaviours/SkipTurnBehaviourTest.scala
@@ -1,29 +1,39 @@
 package model.rules.behaviours
 
-import engine.events.persistent.LoseTurnEvent
+import engine.events.consumable.{ConsumableGameEvent, SkipTurnEvent}
+import engine.events.persistent.{LoseTurnEvent, PersistentGameEvent}
 import mock.MatchMock
 import model.game.GameStateExtensions.MutableStateExtensions
 import model.game.{Game, MutableGameState}
 import model.rules.operations.Operation
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class SkipTurnBehaviourTest extends AnyFlatSpec with Matchers {
+class SkipTurnBehaviourTest extends AnyFlatSpec with Matchers with BeforeAndAfterEach {
 
   behavior of "SkipTurnBehaviourTest"
 
-  it should "skip a turn and check if the game state has been altered correctly" in {
-    val game: Game = MatchMock.default
-    val event = LoseTurnEvent(game.currentState.currentPlayer, game.currentState.currentTurn, game.currentState.currentCycle)
-    val state: MutableGameState = game.currentState
+  val game: Game = MatchMock.default
+  val state: MutableGameState = game.currentState
 
-    state.submitEvent(event)
+  val skipTurnEvent: ConsumableGameEvent = SkipTurnEvent(game.currentState.currentPlayer, game.currentState.currentTurn, game.currentState.currentCycle)
+  val loseTurnEvent: PersistentGameEvent = LoseTurnEvent(game.currentState.currentPlayer, game.currentState.currentTurn, game.currentState.currentCycle)
 
+  override protected def beforeEach(): Unit = {
+    state.submitEvent(skipTurnEvent)
     val operationSequence: Seq[Operation] = SkipTurnBehaviour().applyRule(state)
     operationSequence.foreach(_.execute(state))
+  }
 
-    // TODO check that a new persistent event LoseTurnEvent is now present
+  it should "check that a turn for the given player has been skipped" in {
     pending
+    game.currentState.currentPlayer.history should contain(loseTurnEvent)
+  }
+
+  it should "still contain the main events since it's not a consumable one" in {
+    pending
+    state.consumableBuffer should contain(skipTurnEvent)
   }
 
 }
