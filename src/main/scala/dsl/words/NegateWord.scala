91001adffaa7277373ef06d259107072c07db175
Samuele Burattini <samuele.burattini@studio.unibo.it>
2020-09-03 17:20:37 +0200

    Hotfix on NegateWord

--- src/main/scala/dsl/words/NegateWord.scala
+++ src/main/scala/dsl/words/NegateWord.scala
@@ -5,5 +5,5 @@ import model.entities.runtime.GameState
 class NegateWord(when: GameState => Boolean) {
   def apply(to: ToWord): NegateWord = this
 
-  def use(refName: String): RefAction = RefAction(when, allow = false, refName)
+  def use(refName: String): RefAction = RefAction(when, allow = false, Set(refName))
 }
