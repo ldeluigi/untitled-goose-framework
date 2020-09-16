398b8694067ecb76bfaef16ad614e7d161263d82
Samuele Burattini <samuele.burattini@studio.unibo.it>
2020-09-16 16:15:07 +0200

    Hotfix on displayQuestion with custom events

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "untitled-goose-framework"
 
 organization := "untitled.goose.framework"
 
-version := "0.3.2"
+version := "0.3.3"
 
 scalaVersion := "2.12.10"
 
--- src/test/scala/untitled/goose/framework/main/GooseGame.scala
+++ src/test/scala/untitled/goose/framework/main/GooseGame.scala
@@ -54,7 +54,7 @@ object GooseGame extends GooseDSL {
   Players start on tile 1
 
   Define event "custom" having (
-    "value" as[Int] value,
+    "value" as[Int] value
     )
 
   Define playerEvent "custom2" having (
@@ -65,9 +65,9 @@ object GooseGame extends GooseDSL {
 
   Players loseTurn priority is 5
 
-  Each turn players are (
+  Each turn players are(
     always allowed to roll 1 movementDice "six-faced" as "roll a dice" priority 5,
-    always allowed to displayQuestion("Title", "Text", "Si" -> customGameEvent("custom")) as "Show dialog" priority 3,
+    always allowed to displayQuestion("Title", "Text", "Si" -> (customGameEvent("custom") :+ ("value", _ => 6))) as "Show dialog" priority 3,
     always allowed to trigger (customGameEvent("custom") :+ ("value", _ => 6)) as "Something" priority 2,
     //always allowed to trigger MakeSteps(10) as "Fai 10 passi" priority 5,
     //always allowed to trigger (customPlayerEvent("custom2", _.currentPlayer) := "asd" -> "ok") as "SomethingP" priority 3
