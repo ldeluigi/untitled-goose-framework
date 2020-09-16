190e0c95be605a2d12fb78a1f9cd465aaf2e84b7
ldeluigi <lucadelu97@gmail.com>
2020-09-16 16:38:16 +0200

    New tuProlog setup

--- src/main/scala/untitled/goose/framework/dsl/prolog/GooseDSLPrologExtension.scala
+++ src/main/scala/untitled/goose/framework/dsl/prolog/GooseDSLPrologExtension.scala
@@ -1,11 +1,19 @@
 package untitled.goose.framework.dsl.prolog
 
+import alice.tuprolog.lib.OOLibrary
+import alice.tuprolog.{Prolog, Struct}
 import untitled.goose.framework.model.entities.runtime.GameState
 
 
 trait GooseDSLPrologExtension {
-  implicit def modelCondition(s: String): GameState => Boolean = {
-    //val solver = SolverFactory.DefaultImpls.solverWithDefaultBuiltins()
-    ???
-  }
+  implicit def modelCondition(s: String): GameState => Boolean =
+    state => {
+      val engine: Prolog = new Prolog()
+      val lib: OOLibrary = engine.getLibrary("alice.tuprolog.lib.OOLibrary").asInstanceOf[OOLibrary]
+      val gameStateId: Struct = Struct.atom("gamestate")
+      lib.register(gameStateId, state)
+
+      lib.unregister(gameStateId)
+    }
+
 }
